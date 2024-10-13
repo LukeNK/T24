43211213536da6368ba17d54e00c980ccc454d5d
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-10-13 21:27:28 +0700

    Basic progress bar

--- web/script.js
+++ web/script.js
@@ -17,7 +17,14 @@ function startGame(s, g, i) {
         // exit game
 
     }
+
     QUEST = SUBJECTS[s][g][i];
+
+    // set progress bar
+    document.getElementById('progress').innerHTML = ''; //clear
+    for (let l0 = 0; l0 < QUEST.questions.length; l0++)
+        document.getElementById('progress').innerHTML +=
+            `<p id="pro${l0}">○</p>`;
 }
 
 /**
@@ -30,6 +37,9 @@ function ask() {
     do {
         n = floor(random() * QUEST.questions.length);
     } while (DONE.includes(n));
+
+    // set progress bar
+    document.getElementById('pro' + n).innerHTML = '◉';
 
     n = QUEST.questions[n]; // the question class
 
@@ -46,3 +56,6 @@ function ask() {
     }
 }
 
+function check() {
+
+}

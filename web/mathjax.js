0a2ecf24d4e8713bf80331afb35a2e86a547ccf4
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2025-03-22 12:20:25 +0700

    Fixed MathJax renderer

--- web/mathjax.js
+++ web/mathjax.js
@@ -10,9 +10,9 @@ window.MathJax = {
     }
 };
 
-(function () {
+window.addEventListener('DOMContentLoaded', () => {
     var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
     script.async = true;
     document.head.appendChild(script);
-})();
+});
